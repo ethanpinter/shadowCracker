02d5b944854001c06ae25afe6fee386c35ce0640
ethanpinter <ethanpinter@psu.edu>
2022-10-01 17:56:17 -0400

    added script

---
+++ rainbowGenerator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import sys
+
+''' TODO
+// get file from args, 
+// loop through each line and hash value, 
+    store to hashmap with hashed value as key, plaintext as value
+// wrap functions with cuda lib
+// clean up
+'''
+
+
